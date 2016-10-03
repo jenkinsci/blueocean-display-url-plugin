f347cf28bf036e322c582bfca2f80776e87f163f
Ivan Meredith <ivan@ivan.net.nz>
2016-10-03 13:57:18 +1300

    Initial commit

---
+++ src/main/java/org/jenkinsci/plugins/blueoceandisplayurl/BlueOceanDisplayURLImpl.java
@@ -0,0 +1,76 @@
+package org.jenkinsci.plugins.blueoceandisplayurl;
+
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.Job;
+import hudson.model.Run;
+import jenkins.model.Jenkins;
+
+import hudson.tasks.test.TestResult;
+import jenkins.branch.MultiBranchProject;
+import org.jenkinsci.plugins.displayurlapi.DisplayURLProvider;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+
+
+/**
+ *`@author Ivan Meredith
+ */
+@Extension
+public class BlueOceanDisplayURLImpl extends DisplayURLProvider {
+    @Override
+    public String getRoot() {
+        Jenkins jenkins = Jenkins.getInstance();
+        if (jenkins == null) {
+            throw new IllegalStateException("Jenkins has not started");
+        }
+        String root = jenkins.getRootUrl();
+        if (root == null) {
+            root = "http://unconfigured-jenkins-location/";
+        }
+        return root + "blue/";
+    }
+
+    @Override
+    public String getRunURL(Run<?, ?> run) {
+        if(run instanceof WorkflowRun) {
+            WorkflowJob job =  ((WorkflowRun) run).getParent();
+            if(job.getParent() instanceof MultiBranchProject) {
+                return getJobURL(((MultiBranchProject) job.getParent()))+ "detail/" +  Util.rawEncode(job.getDisplayName()) + "/" + run.getNumber() + "/";
+            }
+        }
+        if(run.getParent() instanceof Job) {
+            Job job = ((Job) run.getParent());
+            return getJobURL(job) + "detail/" + Util.rawEncode(job.getDisplayName()) + "/" + run.getNumber() + "/";
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getChangesURL(Run<?, ?> run) {
+        return getRunURL(run) + "changes";
+    }
+
+    public String getJobURL(MultiBranchProject<?, ?> project) {
+        String jobPath = Util.rawEncode(project.getFullName());
+
+        return getRoot() + "organizations/jenkins/pipelines/" + jobPath + "/";
+    }
+    @Override
+    public String getJobURL(Job<?, ?> project) {
+        String jobPath;
+        if(project.getParent() instanceof MultiBranchProject) {
+            jobPath = Util.rawEncode(project.getParent().getFullName());
+        } else {
+            jobPath = Util.rawEncode(project.getFullName());
+        }
+
+        return getRoot() + "organizations/jenkins/pipelines/" + jobPath + "/";
+    }
+
+    @Override
+    public String getTestUrl(TestResult result) {
+        return getRunURL(result.getRun()) + "/tests";
+    }
+}
